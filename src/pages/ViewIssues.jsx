1f37242f203e253a119d82d06ea80b42f9847162
Subitha17 <subis0420@gmail.com>
2025-10-27 17:10:32 +0530

    update commit

--- src/pages/ViewIssues.jsx
+++ src/pages/ViewIssues.jsx
@@ -15,7 +15,7 @@ export default function ViewIssues() {
   const fetchIssues = async () => {
     try {
       setLoading(true);
-      const res = await fetch("http://localhost:5000/api/issues");
+      const res = await fetch("http://community-issue-tracker-frontend.vercel.app/api/issues");
       if (!res.ok) throw new Error("Failed to fetch issues");
       const data = await res.json();
       setIssues(data);
--- src/pages/reportissue.jsx
+++ src/pages/reportissue.jsx
@@ -17,7 +17,7 @@ export default function ReportIssue() {
     const issueData = { title, description, category };
 
     try {
-      const response = await fetch("http://localhost:5000/api/issues", {
+      const response = await fetch("http://community-issue-tracker-frontend.vercel.app/api/issues", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(issueData),
